8154897123901692d874e91a70d07cc7a3416a88
Mrinal Maheshwari <maheshwari.mrinal@gmail.com>
2020-05-21 13:19:44 +0530

    Initial Commit

---
+++ ekanek-assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ekanek-assignment
+//
+//  Created by Mrinal Maheshwari on 21/05/20.
+//  Copyright © 2020 Mrinal Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
