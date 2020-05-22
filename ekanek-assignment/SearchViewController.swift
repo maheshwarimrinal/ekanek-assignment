2009a49d2782f2fb27efcc3f2d9bcb4d5116e49f
Mrinal Maheshwari <maheshwari.mrinal@gmail.com>
2020-05-23 02:49:59 +0530

    Infinite scrolling added

--- ekanek-assignment/SearchViewController.swift
+++ ekanek-assignment/SearchViewController.swift
@@ -28,14 +28,20 @@ struct Photo: Codable {
     let ispublic, isfriend, isfamily: Int
 }
 
+//extension SearchViewController : UICollectionViewFlowLayout{
+//}
 
-class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class SearchViewController: UIViewController{
+    
+    var isLoading : Bool = false
+    var numberOfItemsPerSection = 30
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        print("\(searchController.searchBar.text ?? "")")
+    }
+    
     
     var search = UISearchController(searchResultsController: nil)
-    
-    var numberOfItemsPerSection = 30
-//    var isPageRefreshing : Bool = false
-//    var page : Int = 0
     
     //Initialising ScrollView for infinite scrolling
     let scrollView : UIScrollView = {
@@ -59,15 +65,22 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
         collectionViewFlowLayout.minimumLineSpacing = 2
         collectionViewFlowLayout.itemSize = CGSize(width: 123, height: 123)
         collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionViewFlowLayout.invalidateLayout()
         
         let imageCollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
         imageCollectionView.backgroundColor = .clear
         imageCollectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: "imageCollectionViewCell")
+//        imageCollectionView.isScrollEnabled = false
 //        imageCollectionView.contentInset = UIEdgeInsets(top: -2, left: -2, bottom: -2, right: -2)
         imageCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageCollectionView
     }()
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        imageCollectionView.collectionViewLayout.invalidateLayout()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,13 +89,14 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
         self.navigationController?.navigationBar.prefersLargeTitles = false
         self.navigationItem.searchController = search
         
+        search.searchResultsUpdater = self
         imageCollectionView.delegate = self
         imageCollectionView.dataSource = self
         
-//        self.view.addSubview(scrollView)
-//        scrollView.addSubview(contentView)
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         
-        self.view.addSubview(imageCollectionView)
+        contentView.addSubview(imageCollectionView)
         parseJSON()
         setLayout()
     }
@@ -90,23 +104,22 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
     func setLayout(){
         NSLayoutConstraint.activate([
             
-            imageCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            imageCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-            imageCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            imageCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.trailingAnchor),
+            
+            imageCollectionView.topAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.topAnchor),
+            imageCollectionView.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor),
+            imageCollectionView.leadingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            imageCollectionView.trailingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.trailingAnchor, constant: -10)
             
         ])
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return numberOfItemsPerSection
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let collectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
-        collectionViewCell.imageView.backgroundColor = .green
-        collectionViewCell.imageView.text = "\(indexPath.row)"
-        return collectionViewCell
     }
     
     func parseJSON(){
@@ -123,23 +136,86 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
            }.resume()
         }
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("\(searchText)lkag;lksh")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("----\(String(describing: searchBar.text))")
+    }
+    
+    func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+
 }
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchResultsUpdating, UISearchBarDelegate{
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfItemsPerSection
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
+        collectionViewCell.imageView.backgroundColor = .green
+        collectionViewCell.imageView.text = "\(indexPath.row)"
+        return collectionViewCell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == numberOfItemsPerSection - 1 && !self.isLoading{
+            loadMoreData()
+        }
+    }
+
+    func loadMoreData() {
+        if !self.isLoading {
+            self.isLoading = true
+            numberOfItemsPerSection = numberOfItemsPerSection + 30
+            DispatchQueue.global().async {
+                // fake background loading task
+                sleep(2)
+                self.parseJSON()
+                DispatchQueue.main.async {
+                    self.imageCollectionView.reloadData()
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+
     
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        let offsetY = scrollView.contentOffset.y
-//        let contentHeight = scrollView.contentSize.height
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+//        if self.isLoading {
+//            return CGSize.zero
+//        } else {
+//            return CGSize(width: collectionView.bounds.size.width, height: 55)
+//        }
+//    }
+
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        if kind == UICollectionView.elementKindSectionFooter {
+//            let aFooterView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "loadingresuableviewid", for: indexPath) as! LoadingReusableView
+//            loadingView = aFooterView
+//            loadingView?.backgroundColor = UIColor.clear
+//            return aFooterView
+//        }
+//        return UICollectionReusableView()
+//    }
+
+//    func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+//        if elementKind == UICollectionView.elementKindSectionFooter {
+//            self.loadingView?.activityIndicator.startAnimating()
+//        }
+//    }
 //
-//        if offsetY > contentHeight - scrollView.frame.size.height {
-//           numberOfItemsPerSection += 30
-//           self.imageCollectionView.reloadData()
+//    func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) {
+//        if elementKind == UICollectionView.elementKindSectionFooter {
+//            self.loadingView?.activityIndicator.stopAnimating()
 //        }
 //    }
-    
-//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-//        print(numberOfItemsPerSection, "+++++++++++")
-//        if indexPath.row == numberOfItemsPerSection - 1{
-//            numberOfItemsPerSection += 30;
-//            imageCollectionView.reloadData()
-//        }
-//        print(numberOfItemsPerSection, "--------")
-//    }
+
+}
